1ceb0f5e7d44241baf2e7c2072f14f9b8fff143a
Zayed Ansari <zayedansari112@gmail.com>
2025-02-04 14:00:54 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Exchange-Rates
+This program checks real-time currency exchange rates using the [ExchangeRatesAPI](https://www.exchangerate-api.com/). It fetches JSON data from [ExchangeRatesAPI](https://www.exchangerate-api.com/) to display the exchange rates for various currencies against a base currency (USD). This tool is perfect for tracking and calculating exchange rates quickly.
+
+- HOW TO USE:
+    Obtain a free api key from https://www.exchangerate-api.com/ amd input in the variable "key"
+
+
+
+# Example:
+
+![alt text](image.png)
+
+
---
+++ src/ExchangeRate.java
@@ -0,0 +1,99 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import java.util.Scanner;
+
+
+public class ExchangeRate {
+
+	static double rate = 1.0;
+	static JSONObject myObject;
+	
+	public static double getRate(String currencyCode) throws Exception {
+		try {
+			String firstPartURL = "https://v6.exchangerate-api.com/v6/";
+
+			// Enter your API key here:
+			String key = "";
+			
+			String thirdPartURL = "/latest/USD";
+			String theURL = firstPartURL + key + thirdPartURL;
+
+			URL url = new URL(theURL);
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			JSONParser jsonParser = new JSONParser();
+			myObject = (JSONObject) jsonParser.parse(br);
+
+			if (currencyCode.equals("USD")) {
+				return 1.0;
+			}
+
+			rate = ((Number) ((JSONObject) myObject.get("conversion_rates")).get(currencyCode)).doubleValue();
+		} 
+		catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return rate;
+	}
+
+	public static void convert() throws Exception {
+
+		Scanner input = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("--Welcome to the Real Time Exchange Rate App--");
+			System.out.println();
+			System.out.println("Select 1 to view exchange rates");
+			System.out.println("Select 2 to convert currency");
+			System.out.println("Select 3 to exit");
+			String choice = input.next();
+			System.out.println();
+
+			if (choice.equals("1")) {
+				System.out.println("Enter currency code:");
+				String currencyCode = input.next().toUpperCase();
+				getRate(currencyCode);
+				System.out.println("Exchange rate for " + currencyCode + " is " + rate);
+			} else if (choice.equals("2")) {
+				System.out.println("Proceeding to currency conversion...");
+
+				System.out.println("Please Enter Current Currency: ");
+				String currentCode = input.next().toUpperCase();
+				double currentRate = getRate(currentCode);
+
+				System.out.println("Please Enter Desired Currency: ");
+				String desiredCode = input.next().toUpperCase();
+				double desiredRate = getRate(desiredCode);
+
+				System.out.println("Please Enter Amount in " + currentCode + ": ");
+				double amount = input.nextDouble();
+
+				double convertedAmount = (amount / currentRate) * desiredRate;
+
+				System.out.printf("Converted Amount: %.2f %s%n", convertedAmount, desiredCode);
+
+			} else if (choice.equals("3")) {
+
+				System.out.println("Exiting the application. Goodbye!");
+				break;
+
+			} else {
+
+				System.out.println("Invalid choice. Please try again.");
+
+			}
+			
+			System.out.println();
+		}
+
+		input.close();
+	}
+
+	public static void main(String[] args) throws Exception {
+    	convert();
+	}
+
+}
